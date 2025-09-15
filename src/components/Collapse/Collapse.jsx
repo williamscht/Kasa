c9ae92e1e84058f37cdb8112cf554752861264c4
Williamscht <williamscht75@gmail.com>
2025-09-15 09:52:52 +0200

    feat: add Collapse (composant) + style page (about)

---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+import "./Collapse.scss"
+import { FaChevronDown } from "react-icons/fa" // tu peux installer react-icons
+
+function Collapse({ title, children }) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <div className="collapse">
+      <div className="collapse-header" onClick={toggleCollapse}>
+        <span>{title}</span>
+        <FaChevronDown className={`icon ${isOpen ? "rotate" : ""}`} />
+      </div>
+      {isOpen && <div className="collapse-content">{children}</div>}
+    </div>
+  )
+}
+
+export default Collapse
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,13 +1,38 @@
 import "./about.scss"
 import Banner from "../../components/Banner/Banner"
 import aboutBanner from "../../assets/about_banner.png"
+import Collapse from "../../components/Collapse/Collapse"
 
 function About() {
   return (
     <div className="about">
-    <Banner image={aboutBanner}/>
+      <Banner image={aboutBanner} />
+      <div className="about-collapses">
+        <Collapse title="Fiabilité">
+          Les annonces postées sur Kasa garantissent une fiabilité totale.
+          Les photos sont conformes aux logements, et toutes les informations
+          sont régulièrement vérifiées par nos équipes.
+        </Collapse>
+
+        <Collapse title="Respect">
+          La bienveillance fait partie des valeurs fondatrices de Kasa.
+          Tout comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
+        </Collapse>
+
+        <Collapse title="Service">
+          La qualité du service est au cœur de notre engagement chez Kasa.
+          Nous veillons à ce que chaque interaction soit empreinte de respect
+          et de bienveillance.
+        </Collapse>
+
+        <Collapse title="Sécurité">
+          La sécurité est la priorité de Kasa. Nous organisons également des
+          ateliers sur la sécurité domestique pour nos hôtes.
+        </Collapse>
+      </div>
     </div>
-)
+  )
 }
 
 export default About
