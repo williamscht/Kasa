0de32fe2068ac44294a726ad966459d98a9c4d33
Williamscht <williamscht75@gmail.com>
2025-09-28 14:16:11 +0200

    add : 404 page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom"
+import "./index.scss"
+
+
 function Error() {
-  return <h1>Erreur 404 - Page introuvable</h1>
+  return (
+    <div className="error">
+      <h1 className="error-code">404</h1>
+      <p className="error-message">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link to="/" className="error-link">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  )
 }
 
 export default Error
